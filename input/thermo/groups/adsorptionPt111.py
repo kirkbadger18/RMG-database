06964d2673b16c9d7010c791d666864501cea64e
Kirk Badger <kirk_badger@brown.edu>
2026-06-05 17:13:34 -0400

    trying to dag the vdW surface site

--- input/thermo/groups/adsorptionPt111.py
+++ input/thermo/groups/adsorptionPt111.py
@@ -4666,8 +4666,8 @@ entry(
     label = "RXvdWBidentate",
     group=
 """
-1 * X u0 p0 c0 {3,S}
-2 X u0 p0 c0 {4,vdW}
+1 X u0 p0 c0 {3,S}
+2 * X u0 p0 c0 {4,vdW}
 3 R!H u0 px c0 {1,S} {5,[S,D]}
 4 R!H u0 px c0 {2,vdW} {5,D}
 5 R!H u0 px cx {3,[S,D]} {4,D}
@@ -4704,8 +4704,8 @@ entry(
     label = "XOCRXO",
     group=
 """
-1 * X u0 p0 c0 {3,S}
-2 X u0 p0 c0 {4,vdW}
+1 X u0 p0 c0 {3,S}
+2 * X u0 p0 c0 {4,vdW}
 3 O u0 p2 c0 {1,S} {5,S}
 4 O u0 p2 c0 {2,vdW} {5,D}
 5 C u0 p0 c0 {3,S} {4,D}
@@ -4742,8 +4742,8 @@ entry(
     label = "XONXO",
     group=
 """
-1 * X u0 p0 c0 {3,S}
-2 X u0 p0 c0 {4,vdW}
+1 X u0 p0 c0 {3,S}
+2 * X u0 p0 c0 {4,vdW}
 3 O u0 p2 c0 {1,S} {5,S}
 4 O u0 p2 c0 {2,vdW} {5,D}
 5 N u0 px cx {3,S} {4,D}
